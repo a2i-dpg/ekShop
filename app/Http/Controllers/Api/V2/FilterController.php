d35677e823adaeed9dac695446ab44798a9546ee
ringku369 <ringku369@gmail.com>
2022-02-03 14:27:44 +0600

    initial commit

---
+++ app/Http/Controllers/Api/V2/FilterController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api\V2;
+
+use App\Http\Resources\V2\BrandCollection;
+use App\Http\Resources\V2\CategoryCollection;
+use App\Models\Brand;
+use App\Models\Category;
+use Cache;
+
+class FilterController extends Controller
+{
+    public function categories()
+    {
+        //if you want to show base categories
+        return Cache::remember('app.filter_categories', 86400, function () {
+            return new CategoryCollection(Category::where('parent_id', 0)->get());
+        });
+    }
+
+    public function brands()
+    {
+        //show only top 20 brands
+        return Cache::remember('app.filter_brands', 86400, function () {
+            return new BrandCollection(Brand::where('top', 1)->limit(20)->get());
+        });
+    }
+
+
+}
---
+++ resources/views/admin/addons/index.blade.php
@@ -0,0 +1,77 @@
+@extends('admin.layouts.app')
+
+@section('content')
+
+    <div class="">
+        <div class="row ">
+            <div class="col-md-6">
+                <div class="nav border-bottom plx-nav-tabs">
+                    <a class="p-3 fs-16 text-reset show active" data-toggle="tab" href="#installed">{{ translate('Installed Addon')}}</a>
+                    <a class="p-3 fs-16 text-reset" data-toggle="tab" href="#available">{{ translate('Available Addon')}}</a>
+                </div>
+            </div>
+            <div class="col-md-6 mt-3 mt-sm-0 text-center text-md-right">
+                <a href="{{ route('addons.create')}}" class="btn btn-primary">{{ translate('Install/Update Addon')}}</a>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="tab-content">
+        <div class="tab-pane fade in active show" id="installed">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <ul class="list-group">
+                                @forelse(\App\Models\Addon::all() as $key => $addon)
+                                    <li class="list-group-item">
+                                        <div class="align-items-center d-flex flex-column flex-md-row">
+                                            <img class="h-60px mb-3 mb-md-0" src="{{ static_asset($addon->image) }}" alt="Image">
+                                            <div class="mr-md-3 ml-md-5">
+                                                <h4 class="fs-16 fw-600">{{ ucfirst($addon->name) }}</h4>
+                                            </div>
+                                            <div class="mr-md-3 ml-0">
+                                                <p><small>{{ translate('Version')}}: </small>{{ $addon->version }}</p>
+                                            </div>
+                                            @if (env('DEMO_MODE') != 'On')
+                                                <div class="mr-md-3 ml-0">
+                                                    <p><small>{{ translate('Purchase code')}}: </small>{{ $addon->purchase_code }}</p>
+                                                </div>
+                                            @endif
+                                            <div class="ml-auto mr-0">
+                                                <label class="plx-switch mb-0">
+                                                    <input type="checkbox" onchange="updateStatus(this, {{ $addon->id }})" <?php if($addon->activated) echo "checked";?>>
+                                                    <span></span>
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </li>
+                                @empty
+                                    <li class="list-group-item">
+                                        <div class="text-center">
+                                            <img class="mw-100 h-200px" src="{{ static_asset('assets/img/nothing.svg') }}" alt="Image">
+                                            <h5 class="mb-0 h5 mt-3">{{ translate('No Addon Installed')}}</h5>
+                                        </div>
+                                    </li>
+                                @endforelse
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="tab-pane fade" id="available">
+            <div class="row" id="available-addons-content">
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+@endsection
+
+@section('script')
+
+@endsection
